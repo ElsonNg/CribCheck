67d215ae7f34445f6267815ae3b8ddeefc8735e3
nickleezx <nickleezx@gmail.com>
2024-10-01 02:38:06 +0800

    Provided simple skeleton and comments
    Comments and abstract methods added for datasetservice class, rough skeleton done for GovtDataset class. note: might need other dataset class if required. Nothing much done to controller.
    
    to be squashed

--- lib/boundary/implementation/govt-dataset-service.ts
+++ lib/boundary/implementation/govt-dataset-service.ts
@@ -1,8 +1,30 @@
+/**
+ * @class GovtDatasetService
+ * 
+ * This class provides datasets from data.gov.sg
+ * It extends the 'DatasetService' abstract class and implements its 'fetchData' and 'parseData' methods.
+ * 
+ * This service fetches and parses the JSON file from data.gov.sg and returns relevant information required for algorithm
+ * 
+ */
+
 import DatasetService from "@/lib/boundary/dataset-service";
 
 
 class GovtDatasetService extends DatasetService {
     
+    constructor(datasetId: string){
+        super("https://data.gov.sg/api/action/datastore_search?resource_id=");
+        this.url.concat(datasetId);
+    }
+
+    async fetchData(url:string): Promise<Record<string, any>>{
+        //to implement
+    }
+
+    async parseData(data: Record<string, any>): any{
+        //to implement
+    }
 }
 
 
--- lib/control/report-controller.ts
+++ lib/control/report-controller.ts
@@ -1,9 +1,16 @@
+import DatasetService from '@/lib/boundary/dataset-service';
 
+/**
+ * The 'ReportController' class is responsible for managing all the datasets required for generating 
+ * report score via our algorithm by interacting with 'GovtDatasetService' and '<insert other dataset>'
+ */
 
 class ReportController {
 
+    private schoolDataset: DatasetService;
+    private healthcareDataset: DatasetService;
+    private publicTransportDataset: DatasetService;
 
-    
 }
 
 
