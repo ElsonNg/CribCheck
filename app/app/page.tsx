cc8f7b84da7b6b7927ba6264f7dd982f30093d36
nickleezx <nickleezx@gmail.com>
2024-10-07 22:57:46 +0800

    Added location search UI
    as titled

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,10 +1,17 @@
+<<<<<<< HEAD
 import Footer from "@/components/general/footer";
 import NavBar from "@/components/general/nav-bar";
 import ScreenContent from "@/components/screens/screen-content";
+=======
+import Footer from "@/components/footer";
+import NavBar from "@/components/nav-bar";
+import SearchLocation from "@/components/searchlocation/search-location-elements"
+>>>>>>> aeae7dc (Added location search UI)
 
 
 export default function AppPage() {
 
+<<<<<<< HEAD
   return (
     <div className="w-full min-h-screen flex flex-col">
       <div className="grow mx-auto w-full max-w-7xl mb-40">
@@ -17,4 +24,15 @@ export default function AppPage() {
     </div>
 
   );
+=======
+    return (
+        <div className="w-full h-screen flex flex-col">
+          <NavBar />
+          <div className="min-height: fit-content flex flex-col justify-center items-center">
+            <SearchLocation />
+          </div>
+          <Footer/>
+        </div>
+      );
+>>>>>>> aeae7dc (Added location search UI)
 }
