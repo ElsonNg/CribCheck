43b3a8f7c780205625393ac1c0337898e737885d
nickleezx <nickleezx@gmail.com>
2024-10-10 22:23:22 +0800

    Rebased + merge so I can test screens
    as titled

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 import Footer from "@/components/general/footer";
 import NavBar from "@/components/general/nav-bar";
 import ScreenContent from "@/components/screens/screen-content";
-=======
-import Footer from "@/components/footer";
-import NavBar from "@/components/nav-bar";
-import SearchLocation from "@/components/searchlocation/search-location-elements"
->>>>>>> aeae7dc (Added location search UI)
 
 
 export default function AppPage() {
 
-<<<<<<< HEAD
   return (
     <div className="w-full min-h-screen flex flex-col">
       <div className="grow mx-auto w-full max-w-7xl mb-40">
@@ -24,15 +17,4 @@ export default function AppPage() {
     </div>
 
   );
-=======
-    return (
-        <div className="w-full h-screen flex flex-col">
-          <NavBar />
-          <div className="min-height: fit-content flex flex-col justify-center items-center">
-            <SearchLocation />
-          </div>
-          <Footer/>
-        </div>
-      );
->>>>>>> aeae7dc (Added location search UI)
 }
--- components/screens/select-location-screen.tsx
+++ components/screens/select-location-screen.tsx
@@ -1,10 +1,12 @@
 import TestScreenButtons from "@/components/ui/test-screen-buttons";
+import SearchLocation from "@/components/searchlocation/search-location-elements"
 
 
 export default function SelectLocationScreen() {
 
     return (<div className="flex flex-col gap-2">
         Choose Location
+        <SearchLocation/>
         <TestScreenButtons/>
     </div>)
 }
